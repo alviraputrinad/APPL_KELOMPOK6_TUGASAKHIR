fbbf0f110f15307e06683b2e8963d55b742299d2
alviraputrinad <48423400+alviraputrinad@users.noreply.github.com>
2020-07-19 09:36:31 +0700

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+
+import java.io.FileNotFoundException;
+import java.time.LocalDate;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Nasty
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+
+    public static void main(String[] args) throws FileNotFoundException {
+        System.out.println("Default Tutor: (ID: 1, password: 00000000)");
+        Menu.appMenu();
+    }
+
+}
